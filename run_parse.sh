99f5487ddebc67238ea569cedd19dacade7d299f
Steven Hung <stevenokmijn@gmail.com>
2015-12-22 15:13:58 +0800

    add curl arguments

--- run_parse.sh
+++ run_parse.sh
@@ -4,7 +4,7 @@ SCHEDULE_LOCAL="./schedule.html"
 SCHEDULE_LOCAL_UTF8="./schedule.utf8.html"
 SCHEDULE_ICAL="./schedule.ical"
 
-curl $SCHEDULE_SOURCE > $SCHEDULE_LOCAL
+curl -s -S -m 10 $SCHEDULE_SOURCE > $SCHEDULE_LOCAL
 #wget -O $SCHEDULE_LOCAL $SCHEDULE_SOURCE
 iconv -f big5 -t utf8 $SCHEDULE_LOCAL > $SCHEDULE_LOCAL_UTF8
 perl ./yyliu_parser.pl $SCHEDULE_LOCAL_UTF8 > $SCHEDULE_ICAL
